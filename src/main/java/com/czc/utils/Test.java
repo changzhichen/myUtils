8b9d4a73f544ad4f9769b2b196e81ecc1494f7eb
changzhichen <1175409926@qq.com>
2020-05-01 10:05:20 +0800

    test

---
+++ src/main/java/com/czc/utils/Test.java
@@ -0,0 +1,11 @@
+package com.czc.utils;
+
+/**
+ * @author changzhichen
+ * @date 2020-05-01 10:01
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
